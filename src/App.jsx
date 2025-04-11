14f33aff5bca60ee4a1082d8d71610d17f78f512
karanx-ig <karankishoreks7@gmail.com>
2025-04-11 16:19:50 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import{ useState } from "react";
+import "./App.css";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  console.log(results,"results");
+
+  return <>
+  <h1>★ Welcome to Spiderverse ★ </h1>
+  <div className="container">
+  <input type="text" placeholder="Enter to Search" 
+  value={search}
+  onChange={(e)=> setSearch(e.target.value)}/>
+
+  <button 
+  onClick={()=>{
+    fetch("/api/search?text="+ search)
+    .then((response) => {
+      response.json()
+      .then((results) => setResults(results))
+      .catch((error) => console.error("Search error !!", error));
+    })
+    .catch((error) => console.error("Search error !!", error));
+  }
+}
+  >Search</button>
+  </div>
+  <div className="results"> 
+  <ul>
+    {
+      results.map((item,index) => (
+        <li key={index}>
+          <a href={item.url} target="_blank">{item.title}</a>
+        </li>
+      ))
+    }
+  </ul>
+    </div>
+  </>;
+}
+
+export default App;
